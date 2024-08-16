f756e8ed82a31352fdbda38b1a3ad9608baf5955
FlorFonseca <florencia.fonseca@correo.ucu.edu.uy>
2024-08-16 14:09:05 -0300

    arregro de ejercicios

--- TAsJS/scriptTA1.js
+++ TAsJS/scriptTA1.js
@@ -1,8 +1,11 @@
+
+/*Manipulacion de cadenas -----------------------*/ 
 function repeatString ( texto, repeticiones){
     for (i=0; i<repeticiones; i++){
         console.log(texto);
     }
 }
+
 
 function reverseString (texto){
 
@@ -10,6 +13,35 @@ function reverseString (texto){
         console.log(texto[i]);
     }
 }
-
 repeatString("hola", 3);
 reverseString("hola");
+
+/*Procesamiento de arreglos -----------------------*/
+
+function removeFromArray ( arreglo, item){
+    arreglo=arreglo.filter((element)=>element!==item);
+    console.log(arreglo);
+}
+removeFromArray(["hola","chau","buenas"], "chau");
+
+
+function getTheTitles(books) {
+    return books.map(libro => libro.titulo);
+}
+const libros = [{titulo: "Harry Potter y la piedra filosofal", autor: "J.K. Rowling"}, {titulo: "Orgullo y prejuicio", autor:"Jane Austen"}];
+const titles = getTheTitles(libros);
+console.log(titles);
+
+
+
+/*Filtrado y Transformación -----------------------*/
+
+function getOdds (nums){
+    return nums.filter(numero => numero % 2 !== 0);/*puedo filtrar elementos del array que cumplan con cierta condición (en este caso que sean impares)*/
+}
+
+let numeros = [2,5,3,6,8,9];
+let impares = getOdds(numeros);
+console.log(impares);
+
+function duplicate()
--- TAsJS/scriptTA3.js
+++
@@ -1,9 +0,0 @@
-function getOdds (nums){
-    return nums.filter(numero => numero % 2 !== 0);/*puedo filtrar elementos del array que cumplan con cierta condición (en este caso que sean impares)*/
-}
-
-let numeros = [2,5,3,6,8,9];
-let impares = getOdds(numeros);
-console.log(impares);
-
-function duplicate()
